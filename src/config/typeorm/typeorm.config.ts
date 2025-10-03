74e91c0e7e63f4644de82be784b1e4f1f4066254
Hijiji <wlals1169@naver.com>
2025-10-03 13:57:22 +0900

    BolckedMember Entity 추가

--- src/config/typeorm/typeorm.config.ts
+++ src/config/typeorm/typeorm.config.ts
@@ -17,36 +17,29 @@ import { ReceiptImg } from '../entities/receipt.Img.entity';
 import { ReviewIngredient } from '../entities/review.ingredient.entity';
 import { Ingredient } from '../entities/ingredient.entity';
 import { MemberDeletion } from '../entities/member.deleteion.entity';
+import { BlockedMember } from '../entities/blocked.member.entity';
 dotenv.config();
 
 export const typeORMConfig = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
   return {
-    //keepConnectionAlive: false,
     type: 'oracle',
     username: 'admin',
     password: 'DTelwjxmxkdla8*',
-    //    connectString: 'dbdesserttime_high', // tnsnames.ora에 있는 alias 사용
     connectString: `(description= (retry_count=20)(retry_delay=3) (address=(protocol=tcps)(port=1522)(host=adb.ap-chuncheon-1.oraclecloud.com)) (connect_data=(service_name=ga0c4cbf63f5084_dbdesserttime_tp.adb.oraclecloud.com)) (security=(ssl_server_dn_match=yes)))`,
+
+    // connectString: 'dbdesserttime_high', // tnsnames.ora에 있는 alias 사용
     // extra: {
-    //   // oracledb에 직접 전달되는 옵션
     //   configDir: '/Users/jeongjimin/Downloads/Wallet_dbdesserttime',
     //   walletLocation: '/Users/jeongjimin/Downloads/Wallet_dbdesserttime',
-    //   walletPassword: 'DTelwjxmxkdla8*', // Autonomous DB는 보통 null
+    //   walletPassword: 'DTelwjxmxkdla8*', // Autonomous DB는 null or oracla cloud password
     // },
 
-    entities: [MemberDeletion, UserInterestDessert, Member, ProfileImg, Like, Review, ReviewImg, QnA, Notice, Accusation, DessertCategory, Point, PointHistory, ReceiptImg, Ingredient, ReviewIngredient],
+    entities: [BlockedMember, MemberDeletion, UserInterestDessert, Member, ProfileImg, Like, Review, ReviewImg, QnA, Notice, Accusation, DessertCategory, Point, PointHistory, ReceiptImg, Ingredient, ReviewIngredient],
 
-    synchronize: false,
     logging: true,
-
-    // extra: {
-    //   // oracledb에 직접 전달되는 옵션
-    //   configDir: '/Users/jeongjimin/Downloads/Wallet_dbdesserttime',
-    //   walletLocation: '/Users/jeongjimin/Downloads/Wallet_dbdesserttime',
-    //   walletPassword: 'DTelwjxmxkdla8*', // Autonomous DB는 보통 null
-    // },
-    // connectString: `(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-chuncheon-1.oraclecloud.com))(connect_data=(service_name=ga0c4cbf63f5084_dbdesserttime_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))`,
-    // connectString: `(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-chuncheon-1.oraclecloud.com))(connect_data=(service_name=ga0c4cbf63f5084_dbdesserttime_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))`,
+    // dropSchema: true,
+    // synchronize: true,
+    //keepConnectionAlive: false,
     //connectTimeout: 30, //30초가 지나면 트랜잭션을 롤백한다.
     // migrations: [process.cwd() + '\\src\\database\\migrations\\*.ts'],
     // migrationsRun: true, //자동적으로 처음 migration이 실행되도록 한다.
