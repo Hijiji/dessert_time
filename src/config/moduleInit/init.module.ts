94861996027d9e8e86a5250b03d60b9710f4e2b1
Hijiji <wlals1169@naver.com>
2025-10-03 15:37:06 +0900

    server init - 재료 초기 데이터삽입로직 추가

--- src/config/moduleInit/init.module.ts
+++ src/config/moduleInit/init.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InitService } from './Init.service';
 import { InitRepository } from './init.repository';
-
+import { Ingredient } from '../entities/ingredient.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([Ingredient])],
   controllers: [],
   providers: [InitService, InitRepository],
   exports: [],
